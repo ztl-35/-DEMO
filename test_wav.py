647c123536164acd5f8053d91f59b2d09387f498
ztl-35 <31171445+ztl-35@users.noreply.github.com>
2021-11-25 09:59:11 +0800

    Update test_wav.py

--- test_wav.py
+++ test_wav.py
@@ -9,6 +9,7 @@ sampwidth = 2
 TIME = 2
 # test git command-ruxian-new
 # test a conflict
+# test ruxian-1
 def save_wave_file(filename, data):
     '''save the date to the wavfile'''
     wf = wave.open(filename, 'wb')
